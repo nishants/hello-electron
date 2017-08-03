d2741f74bafcc46ba5ca0772a7e5e0eff7d6f324
nishant <nishant.singh87@gmail.com>
2017-08-03 09:23:51 +0530

    download file.

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var electron = require('electron');
-const {app, BrowserWindow} = electron;
+var request = require('request');
+var fs = require('fs');
+
+const {app, BrowserWindow, ipcMain} = electron;
 const path = require('path')
 const url = require('url')
+
+var downloadListener;
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
@@ -22,7 +28,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  // win.webContents.openDevTools()
+   win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -38,6 +44,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 // Quit when all windows are closed.
+downloadFile("http://nishants.site/dist.zip", "/Users/dawn/Desktop/downloaded/dist.zip");
 app.on('window-all-closed', () => {
   app.quit()
 })
@@ -47,5 +54,57 @@ app.on('activate', () => {
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
-  }
+    global.myClient = {name: "Zinc"};
+
+}
 })
+
+function downloadFile(file_url , targetPath){
+  // Save variable to know progress
+  var received_bytes = 0;
+  var total_bytes = 0;
+
+  var req = request({
+    method: 'GET',
+    uri: file_url
+  });
+
+  var out = fs.createWriteStream(targetPath);
+  req.pipe(out);
+
+  req.on('response', function ( data ) {
+    // Change the total bytes value to get progress later.
+    total_bytes = parseInt(data.headers['content-length' ]);
+  });
+
+  req.on('data', function(chunk) {
+    // Update the received bytes
+    received_bytes += chunk.length;
+
+    showProgress(received_bytes, total_bytes);
+  });
+
+  req.on('end', function() {
+    alert("File succesfully downloaded");
+  });
+}
+
+function showProgress(received,total){
+  var percentage = (received * 100) / total;
+  console.log(percentage + "% | " + received + " bytes out of " + total + " bytes.");
+  downloadListener && downloadListener.showProgress(received,total);
+}
+ipcMain.on('get-downloader', (event, arg) => {
+  // Print 1
+  console.log(arg);
+ downloadListener = {
+   showProgress: function(received,total){
+     event.sender.send('data-received', {
+       received : received,
+       total    :total
+     });
+   }
+ };
+// Reply on async message from renderer process
+event.sender.send('you-are-listening', "getting updates ?");
+});
